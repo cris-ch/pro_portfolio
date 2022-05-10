7e430f9f38bfbc47ebf6ebbdc43f0ec23d42c2b7
Cristian <cristian.christensen@gmail.com>
2022-05-10 18:02:44 +0200

    added navbar links and styling

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -4,11 +4,19 @@ import { images } from '../../constants/index'
 
 const Navbar = () => {
   return (
-    <nav>
-      <div>
-        <img src={images.logo} alt="logo" />
+    <nav className='app__navbar'>
+      <div className='app__navbar-logo'>
+        <img src={images.logo2} alt="logo" />
       </div>
-
+      <ul className='app__navbar-links'>
+        {['Home', 'About', 'Work', 'Skills', 'Contact'].map((item) => (
+          <li className='app__flex p-text' key={`link-${item}`}>
+            <div>
+              <a href={`#${item}`}>{item}</a>
+            </div>
+          </li>
+        ))}
+      </ul>
     </nav>
   )
 }
