3f90334762027b2d56b4e0abaca53377f558ec1e
Cristian <cristian.christensen@gmail.com>
2022-05-11 11:32:27 +0200

    made navbar responsive

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,24 +1,50 @@
-import React from 'react'
-import './Navbar.scss'
-import { images } from '../../constants/index'
+import React, { useState } from "react";
+import { HiMenuAlt4, HiX } from "react-icons/hi";
+import { motion } from "framer-motion";
+
+import "./Navbar.scss";
+import { images } from "../../constants/index";
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
   return (
-    <nav className='app__navbar'>
-      <div className='app__navbar-logo'>
-        <img src={images.logo2} alt="logo" />
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.logo} alt="logo" />
       </div>
-      <ul className='app__navbar-links'>
-        {['Home', 'About', 'Work', 'Skills', 'Contact'].map((item) => (
-          <li className='app__flex p-text' key={`link-${item}`}>
+      <ul className="app__navbar-links">
+        {["Home", "About", "Work", "Skills", "Contact"].map((item) => (
+          <li className="app__flex p-text" key={`link-${item}`}>
             <div>
               <a href={`#${item}`}>{item}</a>
             </div>
           </li>
         ))}
       </ul>
-    </nav>
-  )
-}
+      <div className="app__navbar-menu">
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
 
-export default Navbar
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.85, ease: "easeOut" }}
+          >
+            <HiX onClick={() => setToggle(false)} />
+            <ul>
+              {["Home", "About", "Work", "Skills", "Contact"].map((item) => (
+                <li key={{ item }}>
+                  <a href={`#${item}`} onClick={() => setToggle(false)}>
+                    {item}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </motion.div>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
