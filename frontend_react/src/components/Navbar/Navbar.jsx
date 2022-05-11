0dbe0aaad88adeec7d6db062e9e351c29f1a7cd9
Cristian <cristian.christensen@gmail.com>
2022-05-11 14:09:59 +0200

    solved issue with navbar links placement

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -11,17 +11,17 @@ const Navbar = () => {
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
-        <img src={images.logo} alt="logo" />
+        <img src={images.logo1} alt="logo" />
       </div>
       <ul className="app__navbar-links">
         {["Home", "About", "Work", "Skills", "Contact"].map((item) => (
           <li className="app__flex p-text" key={`link-${item}`}>
-            <div>
+            <div />
               <a href={`#${item}`}>{item}</a>
-            </div>
           </li>
         ))}
       </ul>
+      
       <div className="app__navbar-menu">
         <HiMenuAlt4 onClick={() => setToggle(true)} />
 
