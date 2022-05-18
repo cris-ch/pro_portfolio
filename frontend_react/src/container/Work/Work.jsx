0b739957a8987dc30f14aa820c44dab5e8a2fc67
Cristian <cristian.christensen@gmail.com>
2022-05-18 22:17:23 +0200

    added categories to portfolio section

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -1,19 +1,39 @@
-import React, {useEffect, useState} from 'react';
-import {AiFillEye, AiFillGithub} from 'react-icons/ai';
-import { motion } from 'framer-motion';
+import React, { useEffect, useState } from "react";
+import { AiFillEye, AiFillGithub } from "react-icons/ai";
+import { motion } from "framer-motion";
 
-import { AppWrap } from '../../wrapper';
-import { urlFor, client } from '../../client';
+import { AppWrap } from "../../wrapper";
+import { urlFor, client } from "../../client";
 
-import './Work.scss';
+import "./Work.scss";
 
 const Work = () => {
+  const [activeFilter, setActiveFilter] = useState("All");
+
+  const handleWorkFilter = () => {};
   return (
     <>
       <h2 className="head-text">
-        My <span>Portfolio</span></h2> 
-    </>
-  )
-}
+        My <span>Portfolio</span>
+      </h2>
 
-export default Work
+      <div className="app__work-filter">
+        {["Web 3", "E-Commerce", "Mern Stack", "CRUD", "All"].map(
+          (item, index) => (
+            <div
+              key={index}
+              onClick={() => handleWorkFilter(item)}
+              className={`app__work-filter-item app_flex p-text ${
+                activeFilter === item ? "item-active" : ""
+              }`}
+            >
+              {item}
+            </div>
+          )
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Work;
